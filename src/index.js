45abdfe46ff7ea3502ffe6f383c6b38681f86b62
xin.zhang <xin.zhang@sca.com.au>
2020-10-06 16:30:51 +1100

    add apollo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
-export.handler =  async () => {
-  return { body: 'hello from lambda' };
+// export.handler =  async () => {
+//   return { body: 'hello from lambda' };
+// };
+
+const { ApolloServer, gql } = require('apollo-server-lambda');
+
+const typeDefs = gql`
+  type Query {
+    user: User
+  }
+
+  type User {
+    id: ID
+    name: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    user: () => ({ id: 123, name: 'John Doe' })
+  }
 };
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+exports.handler = server.createHandler();
 
